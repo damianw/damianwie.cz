6f1af47ce623d9288a7a7fc3cfd0bdd84651a919
Damian Wieczorek <damianw@umich.edu>
2014-02-10 19:30:14 -0500

    added awesome folder effect

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,11 @@
-$(function() {
-
+$( document ).ready(function() {
+  $('.flipper').on('click', function() {
+    document.querySelector('.flip-container').classList.toggle('enter');
+    document.querySelector('.container').classList.toggle('visible');
+    document.querySelector('.vertical.flip-container').classList.toggle('flip'); 
+    document.querySelector('.bottom.separator').classList.toggle('opened');
+    document.querySelector('.top.separator').classList.toggle('opened');
+    document.querySelector('body').classList.toggle('opened');
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  });
 });
